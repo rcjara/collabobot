07fcbd542365eb2b4744636e40b5b4f32b629d1c
Rhea Jara <rhea.is.exploring@gmail.com>
2024-02-02 11:44:16 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use axum::extract::State;
 use axum::{routing::get, Router};
-use color_eyre::eyre::{ErrReport, Result, WrapErr};
-use std::{error::Error, net::SocketAddr};
-use surrealdb::engine::any::connect;
+use color_eyre::eyre::{Result, WrapErr};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+use std::sync::Arc;
+use surrealdb::engine::any::Any;
 use surrealdb::opt::auth::Root;
+use surrealdb::opt::IntoResource;
+use surrealdb::sql::Datetime;
+use surrealdb::{engine::any::connect, Surreal};
 use tracing::{event, instrument, Level};
 
 fn log_and_panic_if_error(result: Result<()>) -> () {
@@ -35,25 +41,8 @@ fn setup_logging() -> Result<()> {
 }
 
 #[instrument]
-async fn apply_migrations() -> Result<()> {
+async fn apply_migrations(db: &Surreal<Any>) -> Result<()> {
     use surrealdb_migrations::MigrationRunner;
-    event!(Level::DEBUG, "connecting to db");
-    let db = connect("ws://localhost:8000").await?;
-    event!(Level::DEBUG, "connected to db");
-
-    event!(Level::DEBUG, "signing into db");
-    // Signin as a namespace, database, or root user
-    db.signin(Root {
-        username: "root",
-        password: "rot",
-    })
-    .await
-    .wrap_err("unable to sign in for migrations")?;
-
-    event!(Level::DEBUG, "signed into db");
-
-    // Select a specific namespace / database
-    db.use_ns("namespace").use_db("database").await?;
 
     // Apply all migrations
     MigrationRunner::new(&db)
@@ -65,34 +54,57 @@ async fn apply_migrations() -> Result<()> {
     Ok(())
 }
 
+#[derive(Serialize, Deserialize)]
+struct Project {
+    project_name: String,
+    created_at: Datetime,
+}
+
+#[derive(Clone)]
+struct AppState {
+    pub db: Surreal<Any>,
+}
+
+impl AppState {
+    async fn initialize() -> Result<Self> {
+        event!(Level::DEBUG, "connecting to db");
+        let db = connect("ws://localhost:8000").await?;
+        event!(Level::DEBUG, "connected to db");
+
+        event!(Level::DEBUG, "signing into db");
+        // Signin as a namespace, database, or root user
+        db.signin(Root {
+            username: "root",
+            password: "rot",
+        })
+        .await
+        .wrap_err("unable to sign in for migrations")?;
+
+        event!(Level::DEBUG, "signed into db");
+
+        // Select a specific namespace / database
+        db.use_ns("namespace").use_db("database").await?;
+        Ok(Self { db })
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let () = setup_logging()?;
-    let () = log_and_panic_if_error(apply_migrations().await);
 
-    // Route all requests on "/" endpoint to anonymous handler.
-    //
-    // A handler is an async function which returns something that implements
-    // `axum::response::IntoResponse`.
+    let appstate = Arc::new(AppState::initialize().await?);
+    let () = log_and_panic_if_error(apply_migrations(&appstate.db).await);
 
-    // A closure or a function can be used as handler.
-    //
-    //
-
-    let app = Router::new().route("/", get(handler));
-
-    // Address that server will bind to.
+    let app = Router::new().route("/", get(handler)).with_state(appstate);
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
-    // Use `hyper::server::Server` which is re-exported through `axum::Server` to serve the app.
     let () = axum::Server::bind(&addr)
-        // Hyper server takes a make service.
         .serve(app.into_make_service())
         .await?;
 
     Ok(())
 }
 
-async fn handler() -> &'static str {
+async fn handler(State(appstate): State<Arc<AppState>>) -> &'static str {
+    let projects = appstate.db.select::<Vec<Project>>("projects");
     "Hello, world!"
 }
