66392ed0f4aa16b748abe11d548cb8bf8174d844
Rhea Jara <rhea.is.exploring@gmail.com>
2024-02-16 17:18:49 -0500

    get a form working to insert tables

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use axum::extract::State;
+use axum::error_handling::HandleError;
+use axum::extract::{Request, State};
+use axum::http::{HeaderMap, StatusCode};
+use axum::response::{Html, IntoResponse};
+use axum::routing::{any, post};
+use axum::Form;
 use axum::{routing::get, Router};
 use color_eyre::eyre::{Result, WrapErr};
 use serde::{Deserialize, Serialize};
+use std::future::IntoFuture;
 use std::net::SocketAddr;
 use std::sync::Arc;
-use surrealdb::engine::any::Any;
+use surrealdb::engine::any::{connect, Any};
 use surrealdb::opt::auth::Root;
-use surrealdb::opt::IntoResource;
 use surrealdb::sql::Datetime;
-use surrealdb::{engine::any::connect, Surreal};
+use surrealdb::Surreal;
 use tracing::{event, instrument, Level};
 
 fn log_and_panic_if_error(result: Result<()>) -> () {
@@ -58,6 +63,12 @@ async fn apply_migrations(db: &Surreal<Any>) -> Result<()> {
 struct Project {
     project_name: String,
     created_at: Datetime,
+    id: surrealdb::sql::Thing,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ProjectForm {
+    project_name: String,
 }
 
 #[derive(Clone)]
@@ -75,10 +86,10 @@ impl AppState {
         // Signin as a namespace, database, or root user
         db.signin(Root {
             username: "root",
-            password: "rot",
+            password: "root",
         })
         .await
-        .wrap_err("unable to sign in for migrations")?;
+        .wrap_err("unable to sign in")?;
 
         event!(Level::DEBUG, "signed into db");
 
@@ -94,8 +105,17 @@ async fn main() -> Result<()> {
 
     let appstate = Arc::new(AppState::initialize().await?);
     let () = log_and_panic_if_error(apply_migrations(&appstate.db).await);
+    //let error_handler = HandleError::new(handler, handle_eyre_error);
 
-    let app = Router::new().route("/", get(handler)).with_state(appstate);
+    let app = Router::new()
+        .route("/", get(handler))
+        .route(
+            "/new-project",
+            get(new_project_form).post(handle_new_project),
+        )
+        .route("/hi", get(hi_handler))
+        .fallback(handler_404)
+        .with_state(appstate);
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let () = axum_server::bind(addr)
         .serve(app.into_make_service())
@@ -104,8 +124,91 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn handler(State(appstate): State<Arc<AppState>>) -> &'static str {
-    let projects = appstate.db.select::<Vec<Project>>("projects");
+async fn handler_404() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, "nothing to see here")
+}
 
-    "Hello, world!"
+async fn new_project_form(
+    State(_appstate): State<Arc<AppState>>,
+    request: Request,
+) -> (StatusCode, Html<String>) {
+    event!(Level::INFO, "GET new-message");
+    (
+        StatusCode::OK,
+        format!(
+            "<p>request: {:?}</p><form action=\"/new-project\" method=\"POST\" enctype=\"application/x-www-form-urlencoded\"><input type=text name=project_name></input></form>",
+
+            request
+        )
+        .into(),
+    )
+}
+
+async fn handle_eyre_error(err: eyre::Error) -> (StatusCode, String) {
+    (
+        StatusCode::INTERNAL_SERVER_ERROR,
+        format!("Something went wrong: {err}"),
+    )
+}
+
+async fn hi_handler(State(_appstate): State<Arc<AppState>>) -> Html<&'static str> {
+    Html("<h3>Hi!</h3>")
+}
+
+async fn handle_new_project(
+    State(appstate): State<Arc<AppState>>,
+    Form(project_form): Form<ProjectForm>,
+) -> (StatusCode, Html<String>) {
+    event!(Level::INFO, "Got a post request to handle a new project");
+    let project: std::result::Result<Vec<Project>, _> =
+        appstate.db.create("projects").content(project_form).await;
+    match project {
+        Ok(projects) => {
+            event!(Level::INFO, "WTF are these projects: {:?}", projects);
+            match projects.first() {
+                Some(project) => (
+                    StatusCode::CREATED,
+                    format!(
+                        "<h1> You created a project: {}</h1><p>created at: {}</p>",
+                        project.project_name, project.created_at
+                    )
+                    .into(),
+                ),
+                None => (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("<p>No project was created</p>").into(),
+                ),
+            }
+        }
+        Err(error) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("<p>There was an error: {:?}</p>", error).into(),
+        ),
+    }
+}
+
+async fn handler(State(appstate): State<Arc<AppState>>) -> (StatusCode, Html<String>) {
+    event!(Level::INFO, "Loading the root of the app");
+    let projects = appstate
+        .db
+        .select::<Vec<Project>>("projects")
+        .into_future()
+        .await;
+    match projects {
+        Ok(projects) => {
+            let string = projects
+                .into_iter()
+                .map(|project| format!("<li>{} &#8212 {}</li>", project.id, project.project_name))
+                .collect::<Vec<_>>()
+                .join("\n");
+            (StatusCode::OK, format!("<ul>{string}</ul>").into())
+        }
+        Err(error) => {
+            event!(Level::ERROR, "error {}", error);
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("<p>There was an error: {:?}</p>", error).into(),
+            )
+        }
+    }
 }
